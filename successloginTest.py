b2ed98e7e6c251b411f60e9ad1f7312a6ca6ef43
nilesh1168 <n.suryawanshi1168@gmail.com>
2019-10-03 11:20:35 +0530

    final tests again

--- successloginTest.py
+++ successloginTest.py
@@ -15,7 +15,7 @@ driver.find_element_by_name("login").click()
 time.sleep(1)
 driver.find_element_by_name("username").send_keys("Nilesh")
 time.sleep(1)
-driver.find_element_by_id("password").send_keys("nil")
+driver.find_element_by_id("password").send_keys("nilesh")
 
 time.sleep(1)
 
