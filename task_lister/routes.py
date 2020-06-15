76d4a36b89882b0b26bb89cc3916e88c40cf1c11
nilesh1168 <n.suryawanshi1168@gmail.com>
2020-06-15 12:02:34 +0530

    added git ignore

--- task_lister/routes.py
+++ task_lister/routes.py
@@ -29,6 +29,7 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username = form.username.data).first()
+        
         if user is None or not user.check_password(form.password.data):
             flash("Invalid Username or Password")
             return redirect(url_for('login'))
