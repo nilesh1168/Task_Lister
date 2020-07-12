7bbfc865aaa059455d9e237991f195badda61ecc
nilesh1168 <n.suryawanshi1168@gmail.com>
2020-07-12 16:21:46 +0530

    minor changes

--- task_lister/config.py
+++ task_lister/config.py
@@ -3,10 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'some_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://user:password@dburl:3306/dbname'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT =  465
     MAIL_USE_SSL = True
-    MAIL_USERNAME = 'Your_Email'
-    MAIL_PASSWORD = 'Your_Password'
+    MAIL_USERNAME = 'eamil@gmail.com'
+    MAIL_PASSWORD = 'Password'
