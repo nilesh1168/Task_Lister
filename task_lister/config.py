1c7f2cc53910084a08602b2cd1186bacc89f0d39
nilesh1168 <n.suryawanshi1168@gmail.com>
2025-01-17 17:34:34 -0500

    updated config; added mail server env

--- task_lister/config.py
+++ task_lister/config.py
@@ -5,7 +5,7 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'some_secret_key'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT =  465
     MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('EMAIL') 
